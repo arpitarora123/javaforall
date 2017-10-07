ca13c11e46ef9e664e0a698d35edae406298dafc
Arpit Arora <arpit.arora123@gmail.com>
2017-10-07 21:19:30 +0530

    First git project(DB demo)

---
+++ database-demo/src/main/java/com/example/database/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.database.databasedemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.database.databasedemo.jdbc.PersonJdbcDao;
+
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	PersonJdbcDao personJdbcDao;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+ 
+		LOGGER.info("Person list {}", personJdbcDao.findAll());
+	}
+}
---
+++ database-demo/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+create table person (
+	id integer,
+	name varchar(255),
+	primary key(id)
+);
+
+insert into person (id, name) values (1, 'Arpit');
