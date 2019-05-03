215d4862ba4662b0108f8876bbfe946a71dcc2e4
arpit <arpit.arora123@gmail.com>
2019-05-03 21:38:02 +0530

    Updated spring demo

--- spring-demo/src/main/java/com/springdemo/repository/StudentRepository.java
+++ spring-demo/src/main/java/com/springdemo/repository/StudentRepository.java
@@ -12,13 +12,14 @@ public class StudentRepository {
 
 	public List<Student> loadStudents() {
 
-		System.out.println("Loading students from repo");
+		System.out.println("Fetching students from repo");
 
 		List<Student> students = new ArrayList<Student>();
 		Student student = new Student();
 		student.setName("Arpit");
 		students.add(student);
 
+		System.out.println("Returning students from repo");
 		return students;
 
 	}
--- spring-demo/src/main/java/com/springdemo/service/ManageStudentService.java
+++ spring-demo/src/main/java/com/springdemo/service/ManageStudentService.java
@@ -3,6 +3,7 @@ package com.springdemo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springdemo.Student;
 import com.springdemo.repository.StudentRepository;
 
 @Service
@@ -12,8 +13,11 @@ public class ManageStudentService {
 	StudentRepository studentRepository;
 
 	public void loadAllStudents() {
-		System.out.println("Loading students from Service");
+		System.out.println("Loading students in Service");
 
-		System.out.println(studentRepository.loadStudents());
+		for (Student student : studentRepository.loadStudents()) {
+			System.out.println("Student Name: " + student.getName());
+		}
+		System.out.println("Loaded students in Service");
 	}
 }
